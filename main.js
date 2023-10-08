c7f8c4d45fc50177caefc6fe943c42752f857693
oles <oles>
2023-10-08 18:31:13 +0300

    final result

--- main.js
+++ main.js
@@ -7,19 +7,17 @@ fs.readFile('./data.json', 'utf8', (err, data) => {
     }
 
     let parsedData = JSON.parse(data);
-    let sumExpenses = 0;
-    let sumIncome = 0;
 
     for (let i = 0; i < parsedData.length; i++) {
-        if (parsedData[i].parent == 'BS2_ExpensesTotal') {
-            sumExpenses = sumExpenses + parsedData[i].value;
+        if (parsedData[i].txt == 'Витрати, усього') {
+            value1 = parsedData[i].value;
         }
-        else if (parsedData[i].parent == 'BS2_IncomeTotal') {
-            sumIncome = sumIncome + parsedData[i].value;
+        else if (parsedData[i].txt == 'Доходи, усього') {
+            value2 = parsedData[i].value;
         }
     }
 
-    const content = `Доходи, усього:${sumIncome.toFixed(4)}\nВитрати, усього:${sumExpenses.toFixed(3)}`;
+    const content = `Доходи, усього:${value2.toFixed(4)}\nВитрати, усього:${value1.toFixed(3)}`;
 
     fs.writeFile('./output.txt', content, err => {
         if (err) {
